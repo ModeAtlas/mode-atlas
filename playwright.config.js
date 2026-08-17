78cd9f7016137dabc7aa64f01f3eb711cd8a6f05
Jack Wright <jackwright@Jacks-MacBook-Air.local>
2026-08-17 18:36:01 +1000

    Update 2.47.1

--- playwright.config.js
+++ playwright.config.js
@@ -4,13 +4,16 @@ const fs = require('fs');
 const systemChromium = process.env.PLAYWRIGHT_CHROMIUM_EXECUTABLE_PATH
   || (fs.existsSync('/usr/bin/chromium') ? '/usr/bin/chromium' : undefined);
 
+const reporters = [['list'], ['html', { open: 'never' }]];
+if (process.env.GITHUB_ACTIONS === 'true') reporters.splice(1, 0, ['github']);
+
 module.exports = defineConfig({
   testDir: './tests',
   timeout: 30_000,
   expect: { timeout: 7_500 },
   fullyParallel: false,
   retries: 0,
-  reporter: [['list'], ['html', { open: 'never' }]],
+  reporter: reporters,
   use: {
     baseURL: 'http://127.0.0.1:4173',
     ...(systemChromium ? { launchOptions: { executablePath: systemChromium } } : {}),
